c2a7ce387ed09a36c77f25fd85101e929ba0ddd2
Eduardo Jorge Nicolau <eduardojorgenicolau@gmail.com>
2020-11-24 19:36:09 -0300

    "Pergunte nome e sobrenome e exiba na tela"

---
+++ semana-2/Pergunte nome e sobrenome e exiba na tela/script.js
@@ -0,0 +1,5 @@
+var sobrenome = window.prompt("Olá! Qual seu sobrenome?");
+
+var nome = window.prompt("E seu nome?");
+
+window.alert("Ou seja, você se chama " + nome + " " + sobrenome + ", certo?");
