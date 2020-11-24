c42bc52708c8dc2d262cb28854f924c9d116a38e
Eduardo Jorge Nicolau <eduardojorgenicolau@gmail.com>
2020-11-24 19:43:58 -0300

    "Pergunte o último sobrenome e exiba em letras maiúsculas, junto do tamanho"

---
+++ semana-2/Pergunte o último sobrenome e exiba em letras maiúsculas, junto do tamanho/script.js
@@ -0,0 +1,5 @@
+var sobrenome = window.prompt("Uma pergunta: qual seu último sobrenome?");
+
+// prettier-ignore
+window.alert("Olá, " + sobrenome.toUpperCase() + ". "
+             + "Seu sobrenome contém " + sobrenome.length + " letras.");
