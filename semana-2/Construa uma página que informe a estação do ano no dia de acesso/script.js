b475d158145478d2bf1e94c47b9e608a75c760c4
Eduardo Jorge Nicolau <eduardojorgenicolau@gmail.com>
2020-11-25 02:03:12 -0300

    gerando o alt das imagens

--- semana-2/Construa uma página que informe a estação do ano no dia de acesso/script.js
+++ semana-2/Construa uma página que informe a estação do ano no dia de acesso/script.js
@@ -6,13 +6,17 @@ var seasonImage = document.getElementById("seasonImage");
 if (currentMonth == 11 || (0 <= currentMonth && currentMonth <= 1)) {
   seasonTitle.innerHTML = "Verão";
   seasonImage.src = "verao.jpg";
+  seasonImage.alt = "Uma bela paisagem de verão";
 } else if (2 <= currentMonth && currentMonth <= 4) {
   seasonTitle.innerHTML = "Outono";
   seasonImage.src = "outono.jpg";
+  seasonImage.alt = "Uma bela paisagem de outono";
 } else if (5 <= currentMonth && currentMonth <= 7) {
   seasonTitle.innerHTML = "Inverno";
   seasonImage.src = "inverno.jpg";
+  seasonImage.alt = "Uma bela paisagem de inverno";
 } else if (8 <= currentMonth && currentMonth <= 10) {
   seasonTitle.innerHTML = "Primavera";
   seasonImage.src = "primavera.jpg";
+  seasonImage.alt = "Uma bela paisagem de primavera";
 }
