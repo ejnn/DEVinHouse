08ff1abbe30742b2604b3590e21867325e35f848
Eduardo Jorge Nicolau <eduardojorgenicolau@gmail.com>
2020-12-09 20:03:28 -0300

    17 - correções pós-esclarecimentos

--- semana-4/17 - Ranking de Livros mais Vendidos.js
+++ semana-4/17 - Ranking de Livros mais Vendidos.js
@@ -1,22 +1,6 @@
-const bubbleUp = (array) => {
-  const biggest = { value: Number.NEGATIVE_INFINITY, index: -1 };
-
-  array.forEach((value, index) => {
-    if (value > biggest.value) {
-      biggest.value = value;
-      biggest.index = index;
-    }
-  });
-
-  array.splice(biggest.index, 1);
-  array.splice(0, 0, biggest.value);
-};
-
-const resposta = (array) => {
-  var array = array.slice();
-
-  for (let i = 1; i <= 3; i++) {
-    bubbleUp(array);
-  }
-  console.log(array);
+const resposta = (primeiro, segundo, terceiro, ...resto) => {
+  console.log("O livro mais vendido: " + primeiro);
+  console.log("2⁰ livro mais vendido: " + segundo);
+  console.log("3⁰ livro mais vendido: " + terceiro);
+  console.log("Os outros livros: " + resto);
 };
