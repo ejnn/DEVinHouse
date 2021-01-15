080ca00458441d36c14cf2fa506bb54f7eb792a9
ejnn <eduardojorgenicolau@gmail.com>
2021-01-15 14:40:02 -0300

    back to work! recapitulando...

--- semana-5/src/ListFilter.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ListFilter = ({}) => {
-  return <input type="text" placeholder="Filtre alunos..." />;
-};
-
-export default ListFilter;
--- semana-5/webpath.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-
-const sourceFolder = "src/";
-const sourcePath = path.resolve(__dirname, sourceFolder);
-
-const buildFolder = "dist/";
-const buildPath = path.resolve(__dirname, buildFolder);
-
-module.exports = { sourcePath, buildPath };
