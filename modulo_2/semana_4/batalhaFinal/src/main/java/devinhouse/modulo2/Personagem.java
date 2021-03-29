df9786899b2ef67947992853c2e10d74c90b0da8
ejnn <eduardojorgenicolau@gmail.com>
2021-03-29 18:44:49 -0300

    exercício 7

---
+++ modulo_2/semana_4/batalhaFinal/src/main/java/devinhouse/modulo2/Personagem.java
@@ -0,0 +1,36 @@
+public abstract class Personagem {
+    private int saude;
+    private int ataque;
+    private int defesa;
+
+    public Personagem(int saude, int ataque, int defesa) {
+	this.saude = saude;
+	this.ataque = ataque;
+	this.defesa = defesa;
+    }
+
+    public void setSaude(int saude) {
+	this.saude = saude;
+    }
+
+    public int getSaude() {
+	return this.saude;
+    }
+
+    public void setAtaque(int ataque) {
+	this.ataque = ataque;
+    }
+
+    public int getAtaque() {
+	return this.ataque;
+    }
+
+    public void setDefesa(int defesa) {
+	this.defesa = defesa;
+    }
+
+    public int getDefesa() {
+	return this.defesa;
+    }
+
+}
