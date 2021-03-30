b2ab2b601f1e94c3f6e819466883f1812cb640af
ejnn <eduardojorgenicolau@gmail.com>
2021-03-30 18:41:55 -0300

    exercício 14

--- modulo_2/semana_4/batalhaFinal/src/main/java/devinhouse/modulo2/Armadilha.java
+++ modulo_2/semana_4/batalhaFinal/src/main/java/devinhouse/modulo2/Armadilha.java
@@ -1,6 +1,11 @@
 package devinhouse.modulo2;
 
+import java.util.Random;
+import java.util.Math;
+
 import devinhouse.modulo2.Atacante;
+import devinhouse.modulo2.Personagem;
+
 import devinhouse.modulo2.enums.Arma;
 
 public class Armadilha implements Atacante {
@@ -11,6 +16,27 @@ public class Armadilha implements Atacante {
     public Armadilha() {
 	this.ataque = 5;
 	this.arma = Arma.ARMADILHA;
+    }
+
+    @Override
+    public void atacar(Personagem alvo) {
+
+	Random random = new Random();
+
+	int rolagem = random.nextInt(10) + 1;
+
+	if (rolagem == 1) {
+	    System.out.printf(“O ataque da armadilha pegou de raspão e você não sofreu dano.%n”);
+	    return;
+	}
+
+	int ataqueTotal = rolagem + this.ataque + this.arma.getAtaque();
+	int dano = Math.max(ataqueTotal - alvo.getDefesa(), 0);
+
+	alvo.setSaude(alvo.getSaude - dano);
+	System.out.printf(“Você sofreu %d de dano e agora possui %d pontos de vida.”,
+			  dano, alvo.getSaude());
+
     }
 
 }
--- modulo_2/semana_4/batalhaFinal/src/main/java/devinhouse/modulo2/enums/Arma.java
+++ modulo_2/semana_4/batalhaFinal/src/main/java/devinhouse/modulo2/enums/Arma.java
@@ -1,14 +1,25 @@
 package devinhouse.modulo2.enums;
 
 public enum Arma {
-    ESPADA,
-    MACHADO,
-    MARTELO,
-    CLAVA,
-    ARCO,
-    BESTA,
-    CAJADO,
-    LIVRO_DE_MAGIAS,
-    ARMADILHA,
-    MACHADO_DUPLO;
+    ESPADA (10),
+    MACHADO (10),
+    MARTELO (10),
+    CLAVA (10),
+    ARCO (10),
+    BESTA (10),
+    CAJADO (10),
+    LIVRO_DE_MAGIAS (10),
+    ARMADILHA (10),
+    MACHADO_DUPLO (10);
+
+    private int ataque;
+
+    private Arma(int ataque) {
+	this.ataque = ataque;
+    }
+
+    public int getAtaque() {
+	return this.ataque;
+    }
+    
 }
