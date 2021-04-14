2f338c3b02b6c85cd47a57b428b85047e3eab96e
jorj <jorj@whirlpool>
2021-04-14 15:14:30 -0300

    exercício 10

---
+++ modulo_2/semana_5/springing/src/main/java/br/com/devinhouse/springing/SpringingApplication.java
@@ -0,0 +1,13 @@
+package br.com.devinhouse.springing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringingApplication.class, args);
+	}
+
+}
